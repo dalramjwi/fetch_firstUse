e489a9c8c0530ac70c4385aa4603b01666cb06fb
dalramjwi <dalramjwi@gmail.com>
2024-06-24 14:17:11 +0900

    server maek and make index.html

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const http = require("http");
 const server = http.createServer(function (req, res) {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello World");
+  if (req.method === "GET") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Hello World");
+  }
+  if (req.method === "POST") {
+  }
 });
 server.listen(3000);
